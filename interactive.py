44e411564f045a48444b64ad18a218428043f0a7
hkolano <hannah.kolano@students.olin.edu>
2017-03-08 23:34:11 -0500

    with comments!

--- interactive.py
+++ interactive.py
@@ -1,9 +1,20 @@
 import pygame
 import time
+'''
+interactive.py
+Sam Eppinger and Hannah Kolano
+March 2017
 
+Created as an interactive game experience for SoftDes Spr17. Draw in the grid
+with colors green (g), blue (b), purple (p), yellow (y), or white (w) to play
+different instruments. hit (m) to play your art.
+'''
 
 class TileArtModel:
-    """ Encodes the game state """
+    """ Encodes the game state.
+    self.columns: list of column objects
+    self.cursor: a cursor object
+    initiates channels to play sound on"""
     def __init__(self, num_columns=16, tile_size=40, border_size=10):
         self.cursor = Cursor((150, 100, 200), 30, 30)
         positions = [border_size]
@@ -22,26 +33,31 @@ class TileArtModel:
         self.channel7 = pygame.mixer.Channel(7)
         self.channel0 = pygame.mixer.Channel(0)
         self.Channels = [self.channel1, self.channel2, self.channel3,
-                        self.channel4, self.channel5, self.channel6,
-                        self.channel7, self.channel0]
+                         self.channel4, self.channel5, self.channel6,
+                         self.channel7, self.channel0]
 
 
     def read_column(self):
+        '''Goes across the board and plays the notes designated by each tile
+        '''
         for column in self.columns:
-            time.sleep(.35)
+            time.sleep(.7)
+            for channel in self.Channels:
+                channel.fadeout(400)
             for i in range(8):
                 channel = self.Channels[i]
                 if column.tiles[i].color == color_dict['green']:
                     channel.play(guitar.note_dict[guitar.note_names[7-i]])
-                # if column.tiles[i].color == color_dict['blue']:
-                #     channel.play(guitar.note_dict[guitar.note_names[7-i]])
-                # if column.tiles[i].color == color_dict['purple']:
-                #     channel.play(guitar.note_dict[guitar.note_names[7-i]])
-                # if column.tiles[i].color == color_dict['yellow']:
-                #     channel.play(guitar.note_dict[guitar.note_names[7-i]])
+                if column.tiles[i].color == color_dict['blue']:
+                    channel.play(trumpet.note_dict[trumpet.note_names[7-i]])
+                if column.tiles[i].color == color_dict['purple']:
+                    channel.play(cello.note_dict[cello.note_names[7-i]])
+                if column.tiles[i].color == color_dict['yellow']:
+                    channel.play(sax.note_dict[sax.note_names[7-i]])
 
 class Column:
-    '''definition of a column'''
+    '''definition of a column
+    self.tiles: a list of tile objects from top to bottom'''
     def __init__(self, color, x, y):
         self.tiles = []
         positions = (10, 60, 110, 160, 210, 260, 310, 360)
@@ -51,7 +67,8 @@ class Column:
 
 
 class Tile:
-    '''definition of a singular tile'''
+    '''definition of a singular tile
+    has a height, width, x position, y position, and color'''
     def __init__(self, color, x, y, width=40, height=40):
         self.width = width
         self.height = height
@@ -61,7 +78,10 @@ class Tile:
 
 
 class Instrument:
-    '''defines attributes of an instrument'''
+    '''defines an Instrument
+    self.note_dict: maps note names to their corresponding sounds
+    self.note_names: a list of note note_names
+    self.a, etc: maps to a sound file. can play with ex guitar.e.play()'''
     def __init__(self, notes_list):
         self.note_dict = dict()
         self.note_names = ['c', 'd', 'e', 'f', 'g', 'a', 'b', 'C']
@@ -79,7 +99,10 @@ class Instrument:
 
 
 class Cursor:
-    """ Encodes the state of the cursor in the game """
+    """ Encodes the state of the cursor in the game
+    has a color, x position, y position, radius, width
+    self.whichcolumn keeps track of the column it's in
+    self.whichrow keeps track of the row it's in"""
     def __init__(self, color, x, y, radius=10, width=0, column=0, row=0):
         self.color = color
         self.x = x
@@ -92,7 +115,7 @@ class Cursor:
 
 
 class TileArtController:
-    '''controls the cursor and the state of the tile'''
+    '''takes all possible inputs and changes objects appropriately'''
     def __init__(self, model):
         self.model = model
 
@@ -127,12 +150,13 @@ class TileArtController:
 
 
 class TileArtWindowView:
-    """ A view of brick breaker rendered in a Pygame window """
+    """ the view, in a pygame window """
     def __init__(self, model, screen):
         self.model = model
         self.screen = screen
 
     def draw(self):
+        '''draws the tiles and the cursor'''
         self.screen.fill(pygame.Color(0, 0, 0))
         for column in self.model.columns:
             for tile in column.tiles:
@@ -156,14 +180,14 @@ if __name__ == '__main__':
     guitar = Instrument(['c6gui.wav', 'd6guit.wav', 'e6guit.wav',
                          'f6guit.wav', 'g6guit.wav', 'a6guit.wav',
                          'b6guit.wav', 'c7guit.wav'])
-    # trumpet = Instrument(['c4trum.wav', 'd4trum.wav', 'e4trum.wav',
-    #                       'f4trum.wav', 'g4trum.wav', 'a4trum.wav',
-    #                       'b4trum.wav', 'c5trum.wav'])
-    # cello = Instrument(['c2cello.wav', 'd2cello.wav', 'e2cello.wav',
-    #                     'f2cello.wav', 'g2cello.wav', 'a2cello.wav',
-    #                     'b2cello.wav', 'c3cello.wav'])
-    # sax = Instrument(['c3sax.wav', 'd3sax.wav', 'e3sax.wav', 'f3sax.wav',
-    #                   'g3sax.wav', 'a3sax.wav', 'b3sax.wav', 'c4sax.wav'])
+    trumpet = Instrument(['c4trum.wav', 'd4trum.wav', 'e4trum.wav',
+                          'f4trum.wav', 'g4trum.wav', 'a4trum.wav',
+                          'b4trum.wav', 'c5trum.wav'])
+    cello = Instrument(['c2cello.wav', 'd2cello.wav', 'e2cello.wav',
+                        'f2cello.wav', 'g2cello.wav', 'a2cello.wav',
+                        'b2cello.wav', 'c3cello.wav'])
+    sax = Instrument(['c3sax.wav', 'd3sax.wav', 'e3sax.wav', 'f3sax.wav',
+                      'g3sax.wav', 'a3sax.wav', 'b3sax.wav', 'c4sax.wav'])
     color_dict = {'green': (0, 255, 0),
                   'blue': (0, 0, 255),
                   'purple': (255, 0, 255),
@@ -187,7 +211,6 @@ if __name__ == '__main__':
             if event.type == pygame.KEYDOWN:
                 controller.handle_keydown_event(event)
         view.draw()
-        # view.read_column()
         time.sleep(.001)
 
     pygame.quit()
